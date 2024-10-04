15f949686873a1d7e67c1a7bd9790236e02497e8
VadimPinyagin <vadimbox85@gmail.com>
2024-10-04 23:04:00 +0300

    Добавляет исправления всех линтеров

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,9 +1,9 @@
-let navMain = document.querySelector('.main-nav');
-let navBurger = document.querySelector('.btn-burger');
+const navMain = document.querySelector('.main-nav');
+const navBurger = document.querySelector('.btn-burger');
 
 navMain.classList.remove('main-nav--nojs');
 
-navBurger.addEventListener('click', function () {
+navBurger.addEventListener('click', () => {
   if (navMain.classList.contains('main-nav--is-close')) {
     navMain.classList.remove('main-nav--is-close');
     navMain.classList.add('main-nav--is-open');
