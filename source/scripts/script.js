31f4d97da63a7fd9c24b4b37bdad9bd8ece376f5
Vadim Pinyagin <vadimbox85@gmail.com>
2024-11-27 19:56:14 +0300

    Правки меню на главной странице

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,14 +1,11 @@
-const navMain = document.querySelector('.main-nav');
-const navBurger = document.querySelector('.btn-burger');
+document.querySelector('main-nav').classList.remove('.main-nav--nojs');
 
-navMain.classList.remove('main-nav--nojs');
+const btnBurger = document.querySelector('.btn-burger');
+const mainNav = document.querySelector('.main-nav');
 
-navBurger.addEventListener('click', () => {
-  if (navMain.classList.contains('main-nav--is-close')) {
-    navMain.classList.remove('main-nav--is-close');
-    navMain.classList.add('main-nav--is-open');
-  } else {
-    navMain.classList.add('main-nav--is-close');
-    navMain.classList.remove('main-nav--is-open');
-  }
-});
+if (btnBurger) {
+  btnBurger.addEventListener('click', () => {
+    mainNav.classList.btnBurger('main-nav--is-open');
+    btnBurger.classList.btnBurger('main-nav--is-close');
+  });
+}
