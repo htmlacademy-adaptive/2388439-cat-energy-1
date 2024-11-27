ac25c01a9bed97917e590b0dd036012e196c6081
Vadim Pinyagin <vadimbox85@gmail.com>
2024-11-27 22:43:10 +0300

    Исправляет замечания по меню и частично по слайдеру. Удаляет лишний шрифт.

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,11 +1,13 @@
-document.querySelector('main-nav').classList.remove('.main-nav--nojs');
+document.querySelector('.main-nav').classList.remove('main-nav--nojs');
 
 const btnBurger = document.querySelector('.btn-burger');
 const mainNav = document.querySelector('.main-nav');
 
+mainNav.classList.add('main-nav--is-close');
+
 if (btnBurger) {
   btnBurger.addEventListener('click', () => {
-    mainNav.classList.btnBurger('main-nav--is-open');
-    btnBurger.classList.btnBurger('main-nav--is-close');
+    mainNav.classList.toggle('main-nav--is-open');
+    mainNav.classList.toggle('main-nav--is-close');
   });
 }
